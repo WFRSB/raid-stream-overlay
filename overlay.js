64c55407f1c3ed17d7367ff11293b4d97e862769
pegasy <pegasy@users.noreply.github.com>
2017-01-05 13:45:02 +0100

    changed data source
    and replaced tabs with spaces and added support for ps4 / xb1 using ?platform=xb1&user=whatever

--- overlay.js
+++ overlay.js
@@ -1,19 +1,19 @@
 var lastUpdate = 0;
-var lastData = [];
 var trialDataURL = 'https://86541c65-ac1b-42e0-8eaa-50c36511bceb.pub.cloud.scaleway.com/api/live/PC/';
+var lastData = [];
 var infoURL = 'Info.txt';
 
 function getInfoText(callback) {
-  $.get(infoURL, function(data) {
-    var nonEmptyLines = data.split('\n').filter(function(n){ return n != undefined && n.length > 0 });
+  $.get(infoURL, function (data) {
+    var nonEmptyLines = data.split('\n').filter(function (n) { return n != undefined && n.length > 0 });
     callback(nonEmptyLines);
   });
 }
 
-function getRaidData(username, callback) {
-  if (lastUpdate + 30 * 1000 < $.now()) {
+function getRaidData(platform, username, callback) {
+  if (lastUpdate + 20 * 1000 < $.now()) {
     lastUpdate = $.now();
-    return $.get(trialDataURL + username, function (data) {
+    return $.get('https://api.trials.wf/api/player/' + platform + '/' + username + '/1', function (data) {
       lastData = data;
       callback(data);
     });
@@ -21,8 +21,17 @@ function getRaidData(username, callback) {
   callback(lastData);
 }
 
-function updateTags(username) {
-  getRaidData(username, function (raidData) {
+function timeToSeconds(time) {
+  if (time === undefined) {
+    return 0;
+  }
+  var a = time.split(":");
+  var seconds = (+a[0]) * 60 * 60 + (+a[1]) * 60 + (+a[2]);
+  return seconds;
+}
+
+function updateTags(platform, username) {
+  getRaidData(platform, username, function (raidData) {
     if (!raidData.length) {
       return $('#cont').attr('class', 'container hidden');
     }
@@ -42,48 +51,46 @@ function updateTags(username) {
     })[0];
 
     var leaderboardGenerated = Date.parse(c.leaderboardGenerated);
-    var currentTimeInSeconds = c.currentTimeInSeconds;
+    var currentTimeInSeconds = timeToSeconds(c.time);
 
     if (c.objective !== 'VICTORY' && c.objective !== 'FAILED') {
-      var raidStarted = new Date(leaderboardGenerated - c.currentTimeInSeconds * 1000);
+      var raidStarted = new Date(leaderboardGenerated - currentTimeInSeconds * 1000);
       var delta = (new Date() - raidStarted) / 1000;
       currentTimeInSeconds = delta;
     }
 
-    var players = c.players.map(function (p) {
-      return p.name;
-    }).slice(1).join(', ');
+    var players = c.players.join(', ');
 
-    $('#Host').text(c.host.name).attr('data-content',c.host.name);
-    $('#Objective').text(c.objective).attr('data-content',c.objective);
-    $('#Time').text(SecondsTohhmmss(Math.round(currentTimeInSeconds))).attr('data-content',SecondsTohhmmss(Math.round(currentTimeInSeconds)));
-    $('#Kills').text(c.kills).attr('data-content',c.kills);
-    $('#Deaths').text(c.deaths).attr('data-content',c.deaths);
+    $('#Host').text(c.host).attr('data-content', c.host);
+    $('#Objective').text(c.objective).attr('data-content', c.objective);
+    $('#Time').text(SecondsTohhmmss(Math.round(currentTimeInSeconds))).attr('data-content', SecondsTohhmmss(Math.round(currentTimeInSeconds)));
+    $('#Kills').text(c.kills).attr('data-content', c.kills);
+    $('#Deaths').text(c.deaths).attr('data-content', c.deaths);
     $('#Players').text('Players: ' + players);
-	$('.value').css('width', c.progressValue + '%');
+    $('.value').css('width', c.progressValue + '%');
     //c.progressValue < 100 ? $('.ui-progressbar-value').addClass('loading') : $('.ui-progressbar-value').removeClass('loading');
   });
 }
 
-function customColour(hexValue){
-	if (!hexValue) {
-		return
-	}
-	var selectDom = $('.host, .time, .objective, .deaths, .kills');
-	var progressC = $('.value');
-	var bigint = parseInt(hexValue, 16);
-    var r = (bigint >> 16) & 255;
-    var g = (bigint >> 8) & 255;
-    var b = bigint & 255;
-	var rbg = 'rgb(' + r + ',' + g + ',' + b + ')'
-	selectDom.css('color', rbg);
-	progressC.css('background-color', rbg);
+function customColour(hexValue) {
+  if (!hexValue) {
+    return
+  }
+  var selectDom = $('.host, .time, .objective, .deaths, .kills');
+  var progressC = $('.value');
+  var bigint = parseInt(hexValue, 16);
+  var r = (bigint >> 16) & 255;
+  var g = (bigint >> 8) & 255;
+  var b = bigint & 255;
+  var rbg = 'rgb(' + r + ',' + g + ',' + b + ')'
+  selectDom.css('color', rbg);
+  progressC.css('background-color', rbg);
 }
 
 function getParameterByName(name, url) {
   name = name.replace(/[\[\]]/g, '\\$&');
   var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-  results = regex.exec(url);
+    results = regex.exec(url);
   if (!results) return null;
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, ' '));
@@ -93,8 +100,8 @@ function updateInfoBox(text) {
   $('.info').text(text).attr('data-content', text);
 }
 
-var SecondsTohhmmss = function(totalSeconds) {
-  var hours   = Math.floor(totalSeconds / 3600);
+var SecondsTohhmmss = function (totalSeconds) {
+  var hours = Math.floor(totalSeconds / 3600);
   var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
   var seconds = totalSeconds - (hours * 3600) - (minutes * 60);
 
@@ -103,13 +110,13 @@ var SecondsTohhmmss = function(totalSeconds) {
 
   var result = (hours < 10 ? '0' + hours : hours);
   result += ':' + (minutes < 10 ? '0' + minutes : minutes);
-  result += ':' + (seconds  < 10 ? '0' + seconds : seconds);
+  result += ':' + (seconds < 10 ? '0' + seconds : seconds);
   return result;
 }
 
 function setupInfoBox(username) {
   getInfoText(function (infoText) {
-    if(username && username.length) infoText.push('Current streamer is ' + username + '!');
+    if (username && username.length) infoText.push('Current streamer is ' + username + '!');
 
     var infoCount = Math.round(Math.random() * (infoText.length - 1));
     updateInfoBox(infoText[infoCount]);
@@ -120,12 +127,13 @@ function setupInfoBox(username) {
   });
 }
 
-function onDocumentReady () {
+function onDocumentReady() {
   var username = getParameterByName('user', window.location.href);
   var colour = getParameterByName('colour', window.location.href);
+  var platform = getParameterByName('platform', window.location.href) || "pc";
   customColour(colour);
   setupInfoBox(username);
-  setInterval(updateTags, 250, username);
+  setInterval(updateTags, 250, platform, username);
 }
 
-$(document).ready(function(){onDocumentReady();});
+$(document).ready(function () { onDocumentReady(); });
